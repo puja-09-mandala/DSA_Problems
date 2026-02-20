cba99383f800fca55d42415f7b78cb59a4ff3b18
Mandala Pujitha <pujithamandala09@gmail.com>
2026-02-20 12:01:23 +0530

    Time: 0 ms (100%), Space: 42.5 MB (48.67%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int reverseBits(int n) {
+        int result = 0;
+        for(int i = 0; i < 32; i++) {
+            result <<= 1;          // shift left
+            result |= (n & 1);     // add last bit
+            n >>>= 1;             
+        }
+        return result;
+    }
+}
