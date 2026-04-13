2f823a71cc4329c6c296cd8603a269a54c914404
Mandala Pujitha <pujithamandala09@gmail.com>
2026-04-13 21:48:32 +0530

    Time: 23 ms (31.83%), Space: 46.9 MB (15.83%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,37 +1,34 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        HashMap<Character,Integer>hp=new HashMap<>();
-       HashMap<Character,Integer>hp1=new HashMap<>();
-       if(s1.length()>s2.length())
-       {
-        return false;
-       }
-       char arr[]=s2.toCharArray();
-        for(int i=0;i<s1.length();i++)
-        {
-            hp1.put(s1.charAt(i),hp1.getOrDefault(s1.charAt(i),0)+1);
+
+        HashMap<Character, Integer> hp = new HashMap<>();
+        HashMap<Character, Integer> hp1 = new HashMap<>();
+
+        // build frequency map of s1
+        for (int i = 0; i < s1.length(); i++) {
+            hp.put(s1.charAt(i), hp.getOrDefault(s1.charAt(i), 0) + 1);
         }
-        for(int i=0;i<s1.length();i++)
-        {
-            hp.put(arr[i],hp.getOrDefault(arr[i],0)+1);
-            if(hp.equals(hp1))
-            {
-              return true;
+
+        int left = 0;
+
+        for (int i = 0; i < s2.length(); i++) {
+            char ch = s2.charAt(i);
+            hp1.put(ch, hp1.getOrDefault(ch, 0) + 1);
+            while (i - left + 1 > s1.length()) {
+                char leftChar = s2.charAt(left);
+                hp1.put(leftChar, hp1.get(leftChar) - 1);
+                if (hp1.get(leftChar) == 0) {
+                    hp1.remove(leftChar);
+                }
+                left++;
+            }
+            if (i - left + 1 == s1.length()) {
+                if (hp.equals(hp1)) {
+                    return true;
+                }
             }
         }
-        for(int i=s1.length();i<s2.length();i++)
-        {
-        hp.put(arr[i],hp.getOrDefault(arr[i],0)+1);
-            char old = arr[i - s1.length()];
-            hp.put(old, hp.get(old) - 1);
-            if(hp.get(old) == 0) {
-                hp.remove(old);
-            }
-            if(hp.equals(hp1))
-            {
-                return true;
-            }
-        }
+
         return false;
     }
 }
