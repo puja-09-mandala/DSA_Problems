7f5fbe31c6f320afeb86216f9788f49d19c5b761
Mandala Pujitha <pujithamandala09@gmail.com>
2026-06-14 17:47:12 +0530

    Time: 4 ms (74.98%), Space: 45.3 MB (49.78%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,33 +1,42 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-            List<Integer>list=new ArrayList<>();
-        for(int i=0;i<nums1.length;i++)
-        {
-            for(int j=0;j<nums2.length;j++)
-            {
-                if(nums1[i]==nums2[j])
-                {
-                   boolean found = false;
 
-for(int k = j + 1; k < nums2.length; k++)
-{
-    if(nums2[k] > nums1[i])
-    {
-        list.add(nums2[k]);
-        found = true;
-        break;
-    }
-}
-if(!found)
-    list.add(-1);
-                    }
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums2.length; i++)
+        {
+            map.put(nums2[i], i);
+        }
+
+        List<Integer> list = new ArrayList<>();
+
+        for(int i = 0; i < nums1.length; i++)
+        {
+            int j = map.get(nums1[i]); // directly get index
+
+            boolean found = false;
+
+            for(int k = j + 1; k < nums2.length; k++)
+            {
+                if(nums2[k] > nums1[i])
+                {
+                    list.add(nums2[k]);
+                    found = true;
+                    break;
                 }
             }
-        int a[]=new int[list.size()];
-        for(int i=0;i<list.size();i++)
-        {
-            a[i]=list.get(i);
+
+            if(!found)
+                list.add(-1);
         }
+
+        int[] a = new int[list.size()];
+
+        for(int i = 0; i < list.size(); i++)
+        {
+            a[i] = list.get(i);
+        }
+
         return a;
     }
 }
