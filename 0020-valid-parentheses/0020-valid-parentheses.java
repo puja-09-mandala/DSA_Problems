b956b65a7d419bd26a168ec6ef97ca0c03dbd53b
Mandala Pujitha <pujithamandala09@gmail.com>
2026-07-17 00:07:13 +0530

    Time: 3 ms (87.07%), Space: 42.9 MB (93.43%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -4,25 +4,23 @@ class Solution {
         for(int i=0;i<s.length();i++)
         {
             char ch=s.charAt(i);
-            if(ch=='(' ||ch=='{' || ch=='[')
+            if(ch=='('||ch=='{'||ch=='[')
             {
                 st.push(ch);
             }
-            else
-            {
+             else
+             {
                 if(st.isEmpty())
                 {
                     return false;
                 }
                 char top=st.pop();
-                if((ch==')' && top!='(') ||
-                (ch=='}' && top!='{') ||
-                (ch==']' &&top!='['))
-                {
-return false;
-                } 
+            if((ch==')'&&top!='(') ||(ch=='}'&& top!='{')||( ch==']'&&top!='['))
+            {
+                return false;
             }
+             }
         }
         return st.isEmpty();
+        }
     }
-}
