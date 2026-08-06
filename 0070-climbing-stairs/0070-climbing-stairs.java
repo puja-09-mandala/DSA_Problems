3b7d9f8e61d74b453cc4764e5b02101bf38acdb1
Mandala Pujitha <pujithamandala09@gmail.com>
2026-08-06 21:27:42 +0530

    Time: 0 ms (100%), Space: 42.2 MB (17.8%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,18 +1,18 @@
 class Solution {
-    public int climbStairs(int n) {
-    //tabulation and small problem to larger part
-        // 2 =1+1=2;
-        // dp[1]=1
-        // dp[0]=1'
-        // dp[2]=dp[1]+dp[0];
-        // dp[3]=dp[i-1]+dp[i-2]=2+1
-        int dp[]=new int[n+1];
-        dp[0]=1;
-        dp[1]=1;
-        for(int i=2;i<=n;i++)
+    int helper(int n,int arr[])
+    {
+        if(n==1) return 1;
+        if(n==2 ) return 2;
+        if(arr[n]!=-1)
         {
-            dp[i]=dp[i-1]+dp[i-2];
+            return arr[n];
         }
-        return dp[n];
+        return arr[n]=helper(n-1,arr)+helper(n-2,arr);
+    }
+    public int climbStairs(int n) {
+        int arr[]=new int[n+1];
+        Arrays.fill(arr,-1);
+       int ans=helper(n,arr);
+       return ans;
     }
 }
